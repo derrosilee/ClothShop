41bafdab8c7941ba2f9677bef28f60b01a87380f
derrosilee <derrossilee@gmail.com>
2023-08-09 10:24:07 +0300

    added password_reset.html page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,5 @@
-from allauth.account.views import ConfirmEmailView
-from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-
+from allauth.account.views import PasswordResetView, LogoutView, LoginView
 from accounts.views import CustomSignupView
 
 urlpatterns = [
@@ -9,5 +7,7 @@ urlpatterns = [
     path('accounts/signup/', CustomSignupView.as_view(), name='account_signup'),
     path('accounts/login/', LoginView.as_view(template_name='account/login.html'), name='account_login'),
     path('accounts/logout/', LogoutView.as_view(), name='account_logout'),
+    path('accounts/password/reset/', PasswordResetView.as_view(template_name='account/password_reset.html'),
+         name='account_reset_password'),
 
 ]
