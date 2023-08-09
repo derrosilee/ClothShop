9fffe43a6fff5fa716dc6c8f9c090d08116ebe70
derrosilee <derrossilee@gmail.com>
2023-08-09 17:16:35 +0300

    added sub categories

--- clothingstore/views.py
+++ clothingstore/views.py
@@ -4,4 +4,5 @@ from .models import Product
 
 def product_list(request):
     products = Product.objects.all()
-    return render(request, 'clothingstore/product_list.html', {'products': products})
+    context = {'products': products}
+    return render(request, 'clothingstore/product_list.html', context)
