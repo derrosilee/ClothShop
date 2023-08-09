235efe3820e7e80a24232762bcebf1fd9c17a1fd
derrosilee <derrossilee@gmail.com>
2023-08-09 16:59:02 +0300

    added product models

--- clothingstore/models.py
+++ clothingstore/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    parent_category = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class Size(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    colors = models.CharField(max_length=100)
+    available_sizes = models.CharField(max_length=100)  # Store available sizes as a comma-separated string
+    sizes = models.ManyToManyField(Size)
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image_1 = models.ImageField(upload_to='product_images/')
+    image_2 = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    image_3 = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    image_4 = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    image_5 = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    image_6 = models.ImageField(upload_to='product_images/', blank=True, null=True)
+
+
+class Stock(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    available_quantity = models.PositiveIntegerField()
