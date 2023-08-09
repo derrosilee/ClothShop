5e8d700f3c8442b81d42a1958434377feed083d9
derrosilee <derrossilee@gmail.com>
2023-08-09 11:56:01 +0300

    added error to show if the email already exist

--- accounts/views.py
+++ accounts/views.py
@@ -1,25 +1,31 @@
+from django import forms
 from django.contrib.auth.models import User
-from allauth.account.views import SignupView, ConfirmEmailView
+from allauth.account.views import SignupView
 from allauth.exceptions import ImmediateHttpResponse
 from allauth.account import signals
-from allauth.utils import get_username_max_length
-from django.core.checks import messages
-from django.shortcuts import render
+from allauth.account.forms import SignupForm
+from django.contrib import messages
 
+class CustomSignupForm(SignupForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("An account with this email already exists.")
+        return email
 
 class CustomSignupView(SignupView):
+    form_class = CustomSignupForm  # Use the custom form class
+
     def form_valid(self, form):
-        email = form.cleaned_data['email']
-        if User.objects.filter(email=email).exists():
-            # Email already exists in the database
-            raise ImmediateHttpResponse(self.response_email_exists())
-        return super().form_valid(form)
+        # Handle successful form submission
+        response = super().form_valid(form)
+        # You can add custom actions here if needed
+        return response
 
-    def response_email_exists(self):
-        # Customize the error message to display to the user
-        error_message = "An account with this email already exists."
-        signals.email_confirmation_sent.send(sender=self.form.email_confirmation_email)
-        self.add_message(self.request, messages.ERROR, error_message)
-        return self.render_to_response(self.get_context_data())
-
-
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add custom context data if needed
+        return context
